78fb5aa69c249072a4b4f71aaadc1eb1f1a5e71c
Gokce <gokmengokces@gmail.com>
2021-07-05 15:43:14 -0700

    model Sequential,layers Dense,Conv2D,MaxPooling2D,Flatten, optimizer SGD, dataset mnist imported

---
+++ deep_learning_CNN_MNIST.py
@@ -0,0 +1,8 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import Input
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense,Conv2D,MaxPooling2D,Flatten
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.datasets import mnist
+
